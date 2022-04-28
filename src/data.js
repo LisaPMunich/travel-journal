fde5bc4f984c6d281298dbd998ce364bc3eaf00a
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-04-28 21:00:55 +0200

    create datapool

--- src/data.js
+++ src/data.js
@@ -1,11 +1,29 @@
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    }
+        title: "Ameland",
+        location: "Holland",
+        googleMapsUrl: "https://goo.gl/maps/DTeWdH1AWaLdzfAg9",
+        startDate: "01 Jun, 2018",
+        endDate: "15 Jun, 2018",
+        description: "Ameland is a municipality and one of the West Frisian Islands off the north coast of the Netherlands. It consists mostly of sand dunes. It is the third major island of the West Frisians. The island has four villages, and one small part-village.",
+        imageUrl: "https://source.unsplash.com/WR8c-HyrQHU"
+    },
+    {
+        title: "YangMinShan National Park",
+        location: "Taiwan",
+        googleMapsUrl: "https://goo.gl/maps/f2F9e4okeNeMWqt57",
+        startDate: "25 Mar, 2000",
+        endDate: "26 Mar, 2000",
+        description: "Yangmingshan National Park is one of the nine national parks in Taiwan, located between Taipei and New Taipei City. It is known for its cherry blossoms, hot springs, sulfur deposits, fumaroles, venomous snakes, and hiking trails, including Taiwan's tallest dormant volcano, Seven Star Mountain.",
+        imageUrl: "https://source.unsplash.com/z4NjlwY4B2Q"
+    },
+    {
+        title: "Neist Point, Isle of Skye",
+        location: "Scotland",
+        googleMapsUrl: "https://goo.gl/maps/rSborusdufeUdof7A",
+        startDate: "10 Oct, 2009",
+        endDate: "11 Oct, 2009",
+        description: "Skye is one of the top locations in Scotland to visit. It is famous for its scenery and landscapes. The Island of Skye is 50 miles long and the largest of the Inner Hebrides. The capital is Portree.",
+        imageUrl: "https://source.unsplash.com/IQJlJ8rDblI"
+    },
 ]
